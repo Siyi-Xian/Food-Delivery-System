dd03d79b7ba84e0c1f69419f0995ab2e280f697e
Nandu, Raj Deepak <rajnandu@iu.edu>
2019-10-13 12:52:58 -0400

    updated jwt verification

--- Backend/jwtverification.js
+++ Backend/jwtverification.js
@@ -3,32 +3,33 @@ const config = require('./config.js');
 
 let checkToken = (req, res, next) => {
   // let token = req.headers
-  console.log(req.body)
-  next()
-  // let token = req.body; // Express headers are auto converted to lowercase
-  // if (token.startsWith('Bearer ')) {
-  //   // Remove Bearer from string
-  //   token = token.slice(7, token.length);
-  // }
+  // console.log(req.body.jwttoken)
+  let token = req.body.jwttoken; // Express headers are auto converted to lowercase
+  
 
-  // if (token) {
-  //   jwt.verify(token, config.secret, (err, decoded) => {
-  //     if (err) {
-  //       return res.json({
-  //         success: false,
-  //         message: 'Token is not valid'
-  //       });
-  //     } else {
-  //       req.decoded = decoded;
-  //       next();
-  //     }
-  //   });
-  // } else {
-  //   return res.json({
-  //     success: false,
-  //     message: 'Auth token is not supplied'
-  //   });
-  // }
+  if (token) {
+    if (token.startsWith('Bearer ')) {
+      // Remove Bearer from string
+      token = token.slice(7, token.length);
+    }
+    jwt.verify(token, config.secret, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Token is not valid'
+        });
+      } else {
+        console.log(decoded)
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: 'Auth token is not supplied'
+    });
+  }
 };
 
 module.exports = {
