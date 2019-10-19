d7072007b4e80de89713873043e3b385d5832f10
pwmcbrid <38295835+pwmcbrid@users.noreply.github.com>
2019-10-19 17:59:21 -0400

    updated sidebar links and added a menu form

--- frontend/src/app/restaurantmenu/restaurantmenu.component.ts
+++ frontend/src/app/restaurantmenu/restaurantmenu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {CookieService} from 'ngx-cookie-service';
+import {LoginService} from '../login.service';
 
 @Component({
   selector: 'app-restaurantmenu',
@@ -7,8 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantmenuComponent implements OnInit {
 
-  constructor() { }
+  menuForm
+  constructor(private loginService: LoginService,
+    private formBuilder: FormBuilder,
+    private cookie: CookieService) {
+      this.menuForm = this.formBuilder.group({
+        name: "",
+        cost: "",
+        description: "",
+        image: ""
+      }) 
+     }
 
+
+  onSubmit(menu){
+    var jwttoken = this.cookie.get("jwttoken")
+    menu["jwttoken"] = jwttoken
+    menu['_id'] = this.cookie.get("restaurant_id")
+    var r = this.loginService.sendRequest(menu, "http://localhost:3000/restaurant/menu");
+    r.subscribe(data => {
+      console.log(data)
+      
+    })
+  }
+  
   ngOnInit() {
   }
 
