b1479a9bf4ddd19122839567c8aab33788024a4e
pwmcbrid <38295835+pwmcbrid@users.noreply.github.com>
2019-10-21 13:54:10 -0400

    update to convert image to base64

--- frontend/src/app/restaurantdetails/restaurantdetails.component.ts
+++ frontend/src/app/restaurantdetails/restaurantdetails.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import {CookieService} from 'ngx-cookie-service';
 import {LoginService} from '../login.service';
-import {toBase64String} from '@angular/compiler/src/output/source_map';
 import {HttpClient} from '@angular/common/http';
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 
 @Component({
   selector: 'app-restaurantdetails',
@@ -30,17 +30,32 @@ export class RestaurantdetailsComponent implements OnInit {
 
   }
 
+  selectedFile: File = null;
+
+  fileAsBase64 = null;
+
+  onFileSelected(event){
+    //this.selectedFile = <File>event.target.files[0];
+    var b64;
+    var reader = new FileReader();
+    reader.onload = function(e){
+      console.log("encoding image")
+      b64 = btoa(e.target.toString());
+    }
+    this.fileAsBase64 = b64;
+  }
+
+
 
   onSubmit(details) {
     var jwttoken = this.cookie.get('jwttoken');
     details['jwttoken'] = jwttoken;
     details['_id'] = this.cookie.get('restaurant_id');
-
+  
     //var mod = .module('app', ['naif-base64']);
 
-    //details['res_image'] = toBase64String.encode(image)
-    details['res_image'] = this.cookie.get('res_image');
-
+    details['reg_image'] = this.fileAsBase64;
+    
     var r = this.loginService.sendRequest(details, 'http://localhost:3000/restaurant/restaurant_details');
     r.subscribe(data => {
       console.log(data);
--- frontend/src/app/restaurantmenu/restaurantmenu.component.ts
+++ frontend/src/app/restaurantmenu/restaurantmenu.component.ts
@@ -23,10 +23,24 @@ export class RestaurantmenuComponent implements OnInit {
      }
 
 
+  fileAsBase64 = null;
+
+  onFileSelected(event){
+    var b64
+    var reader = new FileReader()
+    reader.onload = function(e){
+      console.log("encoding images")
+      b64 = btoa(e.target.toString());
+    }
+    this.fileAsBase64 = b64;
+  }
+
+
   onSubmit(menu){
     var jwttoken = this.cookie.get("jwttoken")
     menu["jwttoken"] = jwttoken
     menu['_id'] = this.cookie.get("restaurant_id")
+    menu['image'] = this.fileAsBase64;
     var r = this.loginService.sendRequest(menu, "http://localhost:3000/restaurant/menu");
     r.subscribe(data => {
       console.log(data)
