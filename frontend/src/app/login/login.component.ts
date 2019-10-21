60d486cd360be430b78cd35635fa0680f9cb68e0
Siyi Xian <xiansiyi@gmail.com>
2019-10-20 22:11:56 -0400

    CSS

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 // import { AuthService } from '../auth.service';
 import {FormBuilder} from '@angular/forms';
-import {LoginService} from '../login.service'
-import { CookieService } from 'ngx-cookie-service';
+import {LoginService} from '../login.service';
+import {CookieService} from 'ngx-cookie-service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,42 +11,43 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class LoginComponent implements OnInit {
   userLoginForm;
-  response: any
+  response: any;
 
   timesSubmitted = 0;
+
   constructor(
     private loginService: LoginService,
     private formBuilder: FormBuilder,
-    private cookie: CookieService) { 
-      this.userLoginForm = this.formBuilder.group({
-        email: "",
-        password: ""
-      })
-    }
-
-  ngOnInit() {
-    
+    private cookie: CookieService) {
+    this.userLoginForm = this.formBuilder.group({
+      email: '',
+      password: ''
+    });
   }
 
-  onSubmit(userData){
-    var r = this.loginService.sendRequest(userData, "http://localhost:3000/authentication/login/user");
+  ngOnInit() {
+
+  }
+
+  onSubmit(userData) {
+    var r = this.loginService.sendRequest(userData, 'http://localhost:3000/authentication/login/user');
     r.subscribe(data => {
-      if(data['auth']){
-        this.cookie.set("jwttoken", data['token']);
+      if (data['auth']) {
+        this.cookie.set('jwttoken', data['token']);
       }
-      
-    })
+
+    });
     // this.cookie.set("userid", "123")
-      // console.log(this.response)
+    // console.log(this.response)
     const payload = {
-      jwttoken: this.cookie.get("jwttoken")
-    }
-    console.log(payload)
-    console.log(this.cookie.get("jwttoken"))
-    r = this.loginService.sendRequest(payload, "http://localhost:3000/verifyotp");
-    r.subscribe(data=>{
-      console.log(data)
-    })
+      jwttoken: this.cookie.get('jwttoken')
+    };
+    console.log(payload);
+    console.log(this.cookie.get('jwttoken'));
+    r = this.loginService.sendRequest(payload, 'http://localhost:3000/verifyotp');
+    r.subscribe(data => {
+      console.log(data);
+    });
   }
 
   // logUserIn(event){
@@ -55,24 +57,24 @@ export class LoginComponent implements OnInit {
   //   if (this.timesSubmitted > 1){
   //     console.log("captcha has been prompted")
   //   }
-    
-    //const target = event.target
-    //console.log(target.srcElement['0'])
-    //const email = target.querySelector('email').value
-    //const password = target.querySelector('password').value
+
+  // const target = event.target
+  // console.log(target.srcElement['0'])
+  // const email = target.querySelector('email').value
+  // const password = target.querySelector('password').value
   // }
-    /*
-    // subscribe is throwing error because the http request to the backend hasn't been established yet
-    this.Auth.validateUserDetails(email, password).subscribe(data => {
-      if(data.success){
-        // direct user to customer view
-      }
-      else{
-        window.alert("Invalid credintials")
-      }
-    })
-    console.log(email, password)
-  }
+  /*
+  // subscribe is throwing error because the http request to the backend hasn't been established yet
+  this.Auth.validateUserDetails(email, password).subscribe(data => {
+    if(data.success){
+      // direct user to customer view
+    }
+    else{
+      window.alert("Invalid credintials")
+    }
+  })
+  console.log(email, password)
+}
 */
 
 }
--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(){
+  }
 
   ngOnInit() {
   }
-
 
 
 }
