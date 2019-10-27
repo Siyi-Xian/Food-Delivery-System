078a6cfcff53a94af34d88fb5145d5a4a528e611
Nandu, Raj Deepak <rajnandu@iu.edu>
2019-10-27 13:46:19 -0400

    Code Ready To be Deployed

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser=require("body-parser");
+var nodemailer = require('nodemailer');
+var ObjectId = require('mongoose').Types.ObjectId; 
+const Bcrypt = require("bcryptjs");
+let jwt = require('jsonwebtoken');
+var path = require('path')
+var http = require('http')
+const cookieParser = require('cookie-parser');
+
+
+var authentication = require('./routes/authenticate')
+var restaurant_portal = require('./routes/restaurant_portal')
+
+var port = process.env.PORT || 8080
+
+var cors = require('cors')
+
+var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ 
+    extended: true
+}));
+app.use(cookieParser());
+
+
+app.use(cors())
+
+// app.use(express.static('dist/fooddeliverysystem'))
+app.use(express.static(__dirname + '/dist/fooddeliverysystem'));
+
+// const server = 
+app.use('/restaurant', restaurant_portal);
+app.use('/authentication', authentication)
+
+app.get("/*", function(req, res){
+    res.sendFile(path.join(__dirname+ '/dist/fooddeliverysystem/index.html'))
+})
+// app.get('*', function(req, res){
+//     res.status(404).send("Page Not Found")
+// })
+
+app.listen(port, function(){
+    console.log("Server listening at port " + port);
+    console.log(this.address().address)
+})
---
+++ src/app/restaurantdetails/restaurantdetails.component.ts
@@ -0,0 +1,74 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {CookieService} from 'ngx-cookie-service';
+import {LoginService} from '../login.service';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-restaurantdetails',
+  templateUrl: './restaurantdetails.component.html',
+  styleUrls: ['./restaurantdetails.component.css']
+})
+export class RestaurantdetailsComponent implements OnInit {
+
+  detailsForm;
+
+  constructor(
+    private httpService:HttpClient,
+    private loginService: LoginService,
+    private formBuilder: FormBuilder,
+    private cookie: CookieService) {
+    this.detailsForm = this.formBuilder.group({
+      name: '',
+      location: '',
+      food_category: '',
+      res_image: '',
+      contact: '',
+      working_hours: ''
+    });
+
+  }
+
+  selectedFile: File = null;
+
+  fileAsBase64 = null;
+
+  onFileSelected(event){
+    //this.selectedFile = <File>event.target.files[0];
+    var b64;
+    var reader = new FileReader();
+    reader.onload = function(e){
+      console.log("encoding image")
+      b64 = btoa(e.target.toString());
+      console.log(b64)
+    }
+    this.fileAsBase64 = b64;
+  }
+
+
+
+  onSubmit(details) {
+    var jwttoken = this.cookie.get('jwttoken');
+    details['jwttoken'] = jwttoken;
+    details['_id'] = this.cookie.get('restaurant_id');
+  
+
+    details['res_image'] = this.fileAsBase64;
+    console.log(details['res_image'])
+    var r = this.loginService.sendRequest(details, '/restaurant/restaurant_details/'+this.cookie.get('restaurant_id'));
+    r.subscribe(data => {
+      console.log(data);
+
+    });
+  }
+
+  ngOnInit() {
+    console.log("fge" + this.cookie.get('restaurant_id'))
+    this.httpService.get('/restaurant/display_details/'+this.cookie.get('restaurant_id')).subscribe(data => {
+      // this.detailsForm = data;
+      console.log(data)
+    });
+  }
+
+
+}
