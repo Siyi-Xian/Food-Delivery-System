e3eca74aa47822924966b29eb1d995e9d347455f
annishah <annishah@iu.edu>
2019-11-02 11:00:03 -0400

    displaydetails pages created

--- src/app/restaurantviewdetails/restaurantviewdetails.component.ts
+++ src/app/restaurantviewdetails/restaurantviewdetails.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import {CookieService} from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-restaurantviewdetails',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantviewdetailsComponent implements OnInit {
 
-  constructor() { }
+   restaurantdetails=[];
+   errorMsg:string;
+   errorFlag:boolean=false;
+  constructor( private http:HttpClient,
+      private cookie:CookieService) { }
 
   ngOnInit() {
+    var url='/display_details/'+ this.cookie.get ('restaurant_id')
+    this.http.get(url)
+
   }
 
 }
