b6a031faa50547367e9864967922c4420f1e53b2
Siyi Xian <xiansiyi@gmail.com>
2019-11-02 17:01:56 -0400

    Restaurant Drop Down

--- src/app/restaurantcurrentorders/restaurantcurrentorders.component.ts
+++ src/app/restaurantcurrentorders/restaurantcurrentorders.component.ts
@@ -12,4 +12,6 @@ export class RestaurantcurrentordersComponent implements OnInit {
   ngOnInit() {
   }
 
+
+
 }
--- src/app/restaurantviewmenu/restaurantviewmenu.component.ts
+++ src/app/restaurantviewmenu/restaurantviewmenu.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {CookieService} from 'ngx-cookie-service';
+import {HttpClient} from '@angular/common/http';
+import {LoginService} from "../login.service";
 
 @Component({
   selector: 'app-restaurantviewmenu',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantviewmenuComponent implements OnInit {
 
-  constructor() { }
+  detailsForm;
+
+  constructor(
+    private httpService:HttpClient,
+    private loginService: LoginService,
+    private formBuilder: FormBuilder,
+    private cookie: CookieService,
+    private http: HttpClient) {
+    this.detailsForm = this.formBuilder.group({
+      name: '',
+      location: '',
+      food_category: '',
+      res_image: '',
+      contact: '',
+      working_hours: ''
+    });
+
+  }
 
   ngOnInit() {
+    console.log("fge" + this.cookie.get('restaurant_id'))
+    this.httpService.get('/restaurant/menu/'+this.cookie.get('restaurant_id')).subscribe(data => {
+      // this.detailsForm = data;
+      console.log(data)
+      if (data != null){
+        this.detailsForm.controls['name'].setValue(data['name'])
+        this.detailsForm.controls['location'].setValue(data['location'])
+        this.detailsForm.controls['food_category'].setValue(data['food_category'])
+        this.detailsForm.controls['contact'].setValue(data['contact'])
+        this.detailsForm.controls['working_hours'].setValue(data['working_hours'])
+      }
+
+
+    });
   }
+
 
 }
