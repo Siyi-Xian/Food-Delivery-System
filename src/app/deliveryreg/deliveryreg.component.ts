f66dfd85693f823a166983a444324b1e8383ca00
Siyi Xian <xiansiyi@gmail.com>
2019-11-17 20:39:19 -0500

    delivery login and registration

--- src/app/deliveryreg/deliveryreg.component.ts
+++ src/app/deliveryreg/deliveryreg.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from "../login.service";
+import {Router} from "@angular/router";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-deliveryreg',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deliveryreg.component.css']
 })
 export class DeliveryregComponent implements OnInit {
+  SERVER_URL = "/authentication/sign_up/delivery";
+  deliveryRegistrationForm;
 
-  constructor() { }
+
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private formBuilder: FormBuilder) {
+    this.deliveryRegistrationForm = this.formBuilder.group({
+      name: '',
+      email: '',
+      password: '',
+      confirmpass: ''
+    })
+  }
 
   ngOnInit() {
+
+  }
+
+  newRest(password, confirmpass){
+    //event.preventDefault()
+
+    if(password == confirmpass){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
+  onSubmit(userData){
+
+
+    if(this.newRest(userData.password, userData.confirmpass) && userData.password != null){
+      var r = this.loginService.sendRequest(userData, this.SERVER_URL);
+      r.subscribe(data => {
+        this.router.navigate(['/login']);
+      })
+    }
+    else{
+      alert("Passwords do not match");
+    }
   }
 
 }
