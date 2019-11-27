81f65804797923da6fc245168f6e8669e2b303b9
annishah <annishah@iu.edu>
2019-11-27 17:42:22 -0500

    Chat

--- src/app/chatfeature/chatfeature.component.ts
+++ src/app/chatfeature/chatfeature.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { PusherService } from '../pusher.service';
+
+
+
+interface Message{
+  text: string;
+  user: string;
+
+}
 
 @Component({
   selector: 'app-chatfeature',
   templateUrl: './chatfeature.component.html',
   styleUrls: ['./chatfeature.component.css']
+  
 })
+
+
+
+
+
+
 export class ChatfeatureComponent implements OnInit {
 
-  constructor() { }
+  
+    text: string;
+    user: string;
+    messages: Array<Message>;
+  
+  constructor(private pusherService: PusherService) { 
+    this.messages = [];
+  }
 
+
+  
+
+
+
+  sendMessage(user: string, text: string) {
+    const message: Message = {
+       user: user,
+       text: text,
+    }
+    this.pusherService.messagesChannel.trigger('client-new-message', message);
+    this.messages.push(message);
+  }
+  
   ngOnInit() {
+
+    this.pusherService.messagesChannel.bind('client-new-message', (message) => {
+      this.messages.push(message);
+    });
   }
 
 }
