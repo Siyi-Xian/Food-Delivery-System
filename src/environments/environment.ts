0e981276e16423d4b75fb450710d1418d653bbe8
annishah <annishah@iu.edu>
2019-11-27 17:44:01 -0500

    Chat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  pusher: {
+    key: '49096276b737a48a5ba1',
+  }
 };
 
 /*
