acd8467e8303cf37b08e353d82c49ce4499ee13b
Aneri <shahaneri107@gmail.com>
2019-12-07 13:57:14 -0500

    Captcha

--- src/app/deliverylogin/deliverylogin.component.ts
+++ src/app/deliverylogin/deliverylogin.component.ts
@@ -3,6 +3,10 @@ import {LoginService} from "../login.service";
 import {FormBuilder} from "@angular/forms";
 import {CookieService} from "ngx-cookie-service";
 import { Router } from '@angular/router';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-deliverylogin',
@@ -10,7 +14,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./deliverylogin.component.css']
 })
 export class DeliveryloginComponent implements OnInit {
-
+  @ViewChild('recaptcha', {static: true}) recaptchaElement: ElementRef;
   deliveryLoginForm;
 
 
@@ -28,10 +32,15 @@ export class DeliveryloginComponent implements OnInit {
   timesSubmitted = 0
 
   ngOnInit() {
+    this.addRecaptchaScript();
   }
 
   onSubmit(userData){
     var r = this.loginService.sendRequest(userData, "/authentication/login/delivery");
+    if(this.recaptchaElement == null){
+      alert('Enter recaptcha');
+      return;
+    }
     r.subscribe(data => {
       console.log(data)
       if(data['auth']){
@@ -44,4 +53,27 @@ export class DeliveryloginComponent implements OnInit {
     })
   }
 
+  addRecaptchaScript(){
+    window['grecaptchaCallback'] = () => {
+      this.renderReCaptcha();
+    }
+
+    (function(d, s, id, obj){
+      var js,fjs = d.getElementsByTagName(s)[0];
+      if(d.getElementById(id)) { obj.renderReCaptcha(); return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://www.google.com/recaptcha/api.js?onload=grecaptchaCallback&amp;render=explicit";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'recaptcha-jssdk', this));
+
+  }
+
+  renderReCaptcha(){
+    window['grecaptcha'].render(this.recaptchaElement.nativeElement, {
+      'sitekey' : '6LdhGcMUAAAAAJ2eG5TNInZtPqRfS2MJE-CVeptA',
+      'callback' : (response) => {
+        console.log(response);
+      }
+    });
+  }
 }
