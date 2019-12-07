f41cacf8cb35cd9614b5793b55dcb26c25c2842e
Aneri <shahaneri107@gmail.com>
2019-12-07 13:58:31 -0500

    Captcha

--- src/app/resturantlogin/resturantlogin.component.ts
+++ src/app/resturantlogin/resturantlogin.component.ts
@@ -3,7 +3,9 @@ import {LoginService} from '../login.service';
 import {FormBuilder} from '@angular/forms';
 import {CookieService} from 'ngx-cookie-service';
 import { Router } from '@angular/router';
-
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-resturantlogin',
@@ -11,7 +13,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./resturantlogin.component.css']
 })
 export class ResturantloginComponent implements OnInit {
-
+  @ViewChild('recaptcha', {static: true}) recaptchaElement: ElementRef;
   restaurantLoginForm;
 
 
@@ -28,10 +30,15 @@ export class ResturantloginComponent implements OnInit {
     }
   timesSubmitted = 0
   ngOnInit() {
+    this.addRecaptchaScript();
   }
 
   onSubmit(userData){
     var r = this.loginService.sendRequest(userData, "/authentication/login/restaurant");
+    if(this.recaptchaElement == null){
+      alert('Enter recaptcha');
+      return;
+    }
     r.subscribe(data => {
       console.log(data)
       if(data['auth']){
@@ -48,12 +55,36 @@ export class ResturantloginComponent implements OnInit {
     })
   }
 
-  logRestIn(event){
-    event.preventDefault()
-    console.log(event)
-    this.timesSubmitted += 1
-    if (this.timesSubmitted > 1){
-      console.log("captcha has been prompted")
+  addRecaptchaScript(){
+    window['grecaptchaCallback'] = () => {
+      this.renderReCaptcha();
     }
+
+    (function(d, s, id, obj){
+      var js,fjs = d.getElementsByTagName(s)[0];
+      if(d.getElementById(id)) { obj.renderReCaptcha(); return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://www.google.com/recaptcha/api.js?onload=grecaptchaCallback&amp;render=explicit";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'recaptcha-jssdk', this));
+
   }
+
+  renderReCaptcha(){
+    window['grecaptcha'].render(this.recaptchaElement.nativeElement, {
+      'sitekey' : '6LdhGcMUAAAAAJ2eG5TNInZtPqRfS2MJE-CVeptA',
+      'callback' : (response) => {
+        console.log(response);
+      }
+    });
+  }
+
+  // logRestIn(event){
+  //   event.preventDefault()
+  //   console.log(event)
+  //   this.timesSubmitted += 1
+  //   if (this.timesSubmitted > 1){
+  //     console.log("captcha has been prompted")
+  //   }
+  // }
 }
