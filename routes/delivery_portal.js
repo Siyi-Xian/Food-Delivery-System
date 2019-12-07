84ea3fb84614f7fd5a53472991bf075cc946c153
Raj Nandu <rajnandu2711@gmail.com>
2019-12-06 21:09:59 -0500

    added endpoints for storing profile, order history and current orders

--- routes/delivery_portal.js
+++ routes/delivery_portal.js
@@ -5,23 +5,34 @@ var ObjectId = require('mongoose').Types.ObjectId;
 let jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 let middleware = require('./jwtverification');
+const multer = require('multer')
 
 var router = express.Router()
 mongoose.connect('mongodb://heroku_wr9z45km:4qlbddem2aer4k5djhcrp5ph3s@ds243717.mlab.com:43717/heroku_wr9z45km', { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 
-router.post('/details', middleware.checkToken, function(req, res){
+const delivery_image_storage = multer.diskStorage({
+    destination: (req, file, callBack)=>{
+        callBack(null, "delivery_images")
+    },
+    filename: (req, file, callBack)=>{
+        callBack(null, file.originalname)
+    }
+})
+var upload_delivery = multer({storage: delivery_image_storage})
+
+router.post('/details', [middleware.checkToken, upload_delivery.single('image')], function(req, res){
     var id = req.body.id
     var data_update = {
         $set: {
             name: req.body.name,
             contact: req.body.contact,
-            street1: req.body.street1,
-            street2: req.body.street2,
-            city: req.body.city,
-            state: req.body.state,
-            zip_code: req.body.zip_code,
-            isBusy: false
+            email: req.body.email,
+            // street2: req.body.street2,
+            // city: req.body.city,
+            // state: req.body.state,
+            // zip_code: req.body.zip_code,
+            // isBusy: false
         }
     }
     db.collection('delivery_data').updateOne({_id: ObjectId(id)}, data_update, function(err, data){
@@ -44,26 +55,93 @@ router.post('/details', middleware.checkToken, function(req, res){
 })
 
 
-router.get('/current_orders:delivery_id', middleware.checkToken, function(req, res){
-    var customer_id = req.params.delivery_id;
-    var projection = {
-        projection: {
-            restaurant_id: 1,
-            customer_id: 1,
-            items: 1,
-            address: 1,
-            status: 1
+router.post("/fullfillorder", middleware.checkToken, function(req, res){
+    var id = req.body.id
+    const new_vals = {
+        $set: {
+            status: "Delivered",
+            time: new Date().toLocaleString()
         }
     }
-    db.collection('order').find({_id: new ObjectId(delivery_id)}, projection, function(err, data){
+    db.collection("order").findOneAndUpdate({_id: new ObjectId(id)}, new_vals, function(err, data){
         if(err){
-            console.log("error")
+            console.log(err);
+            res.json({message: "Error"});
+        }
+        else{
+            res.json({message:"success"});
+        }
+    })
+})
+
+
+router.get("/display_details/:id", middleware.checkToken, function(req, res){
+    var id = req.params.id
+    console.log(id)
+    var d = {
+        projection:{
+            _id: 1,
+            name: 1,
+            email: 1,
+            contact: 1,
+        }
+
+    }
+    db.collection("delivery_data").findOne({_id: ObjectId(id)}, d, function(err, result){
+        if (err){
+            console.log(err)
             res.json({
-                message: "No Orders"
+                message: "Failed"
             })
+            return
+        }
+        else{
+            res.json({
+                result
+            })
+            return
+        }
+    })
+});
+
+
+router.get("/order_history/:delivery_id", middleware.checkToken, function(req, res){
+    var id = req.params.delivery_id
+    console.log(req.params.delivery_id)
+    db.collection("order").find({delivery_id: id, status: "Delivered"}).toArray(function(err, data){
+        if (err){
+            console.log(err)
+            res.json({
+                message: "Failed"
+            })
+            return;
         }
         else{
             res.json(data)
+            return;
+        }
+    })
+})
+
+router.get("/current_orders/:delivery_id", middleware.checkToken, function(req, res){
+    var id = req.params.delivery_id
+    var query = {
+        delivery_id: id,
+        status: "On the way!"
+    }
+    db.collection("order").find(query).toArray(function(err, data){
+        if (err){
+            console.log(err)
+            res.json({
+                message: "Failed"
+            })
+            return;
+        }
+        else{
+            console.log("Success")
+            console.log(data)
+            res.json(data)
+            return;
         }
     })
 })
