5cde8947634f82fc3093ef9b857673eaa8437d50
Siyi Xian <xiansiyi@gmail.com>
2019-12-08 21:02:33 -0500

    Location detil

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {CookieService} from "ngx-cookie-service";
 
 
 @Component({
@@ -15,29 +17,41 @@ export class OrderComponent implements OnInit {
   lat = 39.172981;
   lng = -86.523010;
 
-  restaurant = [
-    {
-      location: {
-        lat: 39.170957,
-        lng: -86.516549,
-        label: 'D',
-        draggable: false
-      }
-    }
-  ]
+  // restaurant = [
+  //   {
+  //     location: {
+  //       lat: 39.170957,
+  //       lng: -86.516549,
+  //       label: 'D',
+  //       draggable: false
+  //     }
+  //   }
+  // ]
+  //
+  // destination: marker = {
+  //   lat: 39.172981,
+  //   lng: -86.523010,
+  //   label: 'R',
+  //   draggable: true
+  // }
+  //
+  // delivery: marker = {
+  //   lat: this.lat,
+  //   lng: this.lng,
+  //   label: 'D',
+  //   draggable: false
+  // }
 
-  destination: marker = {
-    lat: 39.172981,
-    lng: -86.523010,
-    label: 'R',
-    draggable: true
-  }
-
-  delivery: marker = {
+  customer_details
+  restaurant_details
+  origin: marker = {
     lat: this.lat,
     lng: this.lng,
-    label: 'D',
     draggable: false
+  }
+
+  constructor(private http: HttpClient,
+              private cookie: CookieService) {
   }
 
   ngOnInit(): void {
@@ -45,11 +59,39 @@ export class OrderComponent implements OnInit {
       navigator.geolocation.getCurrentPosition(pos => {
         this.lng = +pos.coords.longitude;
         this.lat = +pos.coords.latitude;
-        this.delivery.lat = this.lat;
-        this.delivery.lng = this.lng;
+        this.origin.lat = this.lat;
+        this.origin.lng = this.lng;
       });
     }
 
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      jwttoken: this.cookie.get("jwttoken")
+    });
+
+    var url = '/restaurant/display_details/' + this.cookie.get('restaurant_id')
+
+    this.http.get(url, {headers}).subscribe(data => {
+      // this.detailsForm = data;
+
+      if (data != null) {
+        this.restaurant_details = data
+        console.log(data)
+      }
+    });
+
+    var customer_id = this.cookie.get('customer_id')
+    var url = '/customer/display_details/' + customer_id
+    // var params = new HttpParams().set("id", customer_id)
+    this.http.get(url, {headers}).subscribe(data => {
+      // console.log(data)
+
+      if (data != null) {
+
+        this.customer_details = data['result']
+        console.log(this.customer_details)
+      }
+    });
 
   }
 }
